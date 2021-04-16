daf6b7c59b044e1953520b84d54485806ad2976d
LqerC <LqerC@users.noreply.github.com>
2021-04-16 20:28:09 +0200

    dev

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -170,6 +170,7 @@ namespace Lifesiml
             this.Controls.Add(this.splitContainer1);
             this.Name = "Form1";
             this.Text = "Projekt 2";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.splitContainer1.Panel1.ResumeLayout(false);
             this.splitContainer1.Panel1.PerformLayout();
             this.splitContainer1.Panel2.ResumeLayout(false);
--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Lifesiml.Logic;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -19,14 +20,14 @@ namespace Lifesiml
             InitializeComponent();
         }
         private ushort res;
-        private bool[,] space; private int row, col;
+        private Core core;
         private Graphics gdi;
-        Random random = new Random();
+       
 
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            GenerateNew();
+            DrawNew();
         }
         public void RunGame()
         {
@@ -37,15 +38,9 @@ namespace Lifesiml
 
             setRes.Enabled = false; setDens.Enabled = false;
             res = (UInt16)setRes.Value;
-            row = screen.Height / res; col = screen.Width / res;
-            space = new bool[col, row];
-            for (int i = 0; i < col; i++)
-            {
-                for (int j = 0; j < row; j++)
-                {
-                    space[i, j] = random.Next((int)setDens.Value) == 0;
-                }
-            }
+            core = new Core(row: screen.Height / res, col: screen.Width / res,(int)setDens.Value);
+            Text = $"Gen № : {core.Gen}";
+
             screen.Image = new Bitmap(screen.Height, screen.Width);
             gdi = Graphics.FromImage(screen.Image);
             //gdi.FillRectangle(Brushes.Cyan, 0, 0, res, res);
@@ -60,53 +55,32 @@ namespace Lifesiml
                 return;
         }
 
-        private void GenerateNew()
+        private void DrawNew()
         {
-            var newArr = new bool[col, row];
+            
             gdi.Clear(Color.Black);
-            for (int i = 1; i < col; i++)
+            var space = core.GetCurrentAge();
+            for (int x = 0;x < space.GetLength(0); x++)
             {
-
-                for (int j = 1; j < row; j++)
+                for (int y = 0; y < space.GetLength(1); y++)
                 {
-                    var CellNumber = GetCells(i, j);
-                    if (!space[i, j] && CellNumber == 3)
-                        newArr[i, j] = true;
-                    else if (space[i, j] && (CellNumber < 2 || CellNumber > 3))
-                        newArr[i, j] = false;
-                    else
-                        newArr[i, j] = space[i, j];
-
-                    if (space[i, j])
-                        gdi.FillRectangle(Brushes.Cyan, i * res, j * res, res, res);
+                    if(space[x,y])
+                        gdi.FillRectangle(Brushes.Cyan, x * res, y * res, res - 1, res - 1);
+                    
                 }
             }
+            
+            
+            
+            
             //
             //
-            space = newArr;
+
             screen.Refresh();
+            Text = $"Gen № : {core.Gen}";
+            core.GenerateNew();
         }
-        private int GetCells(int x, int y)
-        {
-            int count = 0;
-            for (int i = -1; i < 2; i++)
-            {
-                for (int j = -1; j < 2; j++)
-                {
-                    var cols = (x + i + col) % col;
-                    var rows = (y + j + row) % row;
-                    var check = cols == x && rows == y;
-                    // space[i, j] = space[cols, rows];
-                    var Alive = space[cols, rows];
-                    if (Alive && !check)
-                    {
-                        count++;
-                    }
-                }
 
-            }
-            return count;
-        }
 
 
         private void runButton_Click(object sender, EventArgs e)
@@ -118,35 +92,33 @@ namespace Lifesiml
         {
             if (!timer1.Enabled)
                 return; ;
-        
-            if(e.Button == MouseButtons.Left)
-                {
-                var x = e.Location.X / res;
-                var y = e.Location.Y / res;
-                var isOK = isOnScreen(x, y);
-                if(isOK)
-                    space[x, y] = true;
-            }
-            if(e.Button == MouseButtons.Right)
+
+            if (e.Button == MouseButtons.Left)
             {
                 var x = e.Location.X / res;
                 var y = e.Location.Y / res;
-                var isOK = isOnScreen(x, y);
-                if (isOK)
-                    space[x, y] = false;
+                core.AddCell(x,y);
             }
-         
-           
+            if (e.Button == MouseButtons.Right)
+            {
+                var x = e.Location.X / res;
+                var y = e.Location.Y / res;
+                core.RemoveCell(x, y);
+            }
+
+
         }
 
-        bool isOnScreen(int x, int y)
-        {
-            return x >= 0 && y >= 0 && x < col && y < row;
-        }
+       
         private void stopButton_Click(object sender, EventArgs e)
         {
             StopGame();
             setRes.Enabled = true; setDens.Enabled = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
         }
     }
 }
